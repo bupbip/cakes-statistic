2da470e9efb64c0420534673cffd21748d65323e
KustikovAD <kustikovad@gnivc.ru>
2024-05-17 20:13:16 +0300

    Рефакторинг кода

--- src/main/java/ru/kustikov/cakes/CakesStatisticApplication.java
+++ src/main/java/ru/kustikov/cakes/CakesStatisticApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class CakesStatisticApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CakesStatisticApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CakesStatisticApplication.class, args);
+    }
 
 }
--- src/main/java/ru/kustikov/cakes/scheduler/ScheduledTasks.java
+++ src/main/java/ru/kustikov/cakes/scheduler/ScheduledTasks.java
@@ -1,7 +1,6 @@
 package ru.kustikov.cakes.scheduler;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
